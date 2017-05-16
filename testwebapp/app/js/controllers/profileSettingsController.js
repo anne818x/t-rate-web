37e1d05c5cbb6230d3d0bab0b6c37c7c0ba449e2
Kaloyan Velikov <kaloyan.velikov@student.stenden.com>
2017-05-16 20:04:56 +0200

    update on settings. still have to implement functions in the controller

--- testwebapp/app/js/controllers/profileSettingsController.js
+++ testwebapp/app/js/controllers/profileSettingsController.js
@@ -1,23 +1,40 @@
-var app = angular.module('myApp', ['ngRoute', 'firebase']);
-
 app.controller('profileSettingsController', function ($scope) {
 
     $scope.locations = ['Leeuwarden (NL)', 'Emmen(NL)', 'Meppel(NL)', 'Assen (NL)', 'Groningen (NL)', 'Bali (ID)', 'South Africa (SA)', 'Qatar (QA)', 'Thailand (TH)'];
     $scope.courses = ['Information Technology', 'Pabo', 'Informatica', 'International Business & Languages', 'Logistiek en Economie', 'Marketing', 'Polymer Engineering', 'International Hospitality Management'];
-    $scope.selectedLocation;
-    $scope.selectedCourse;
+    $scope.currentUser = firebase.auth().currentUser;
 
-    $scope.dropboxitemselected = function () {
+
+    //retrieve user data
+    if ($scope.currentUser != null) {
+        $scope.name = $scope.currentUser.displayName,
+            $scope.email = $scope.currentUser.email,
+            $scope.photoUrl = $scope.currentUser.photoURL,
+            $scope.emailVerified = $scope.currentUser.emailVerified,
+            $scope.uid = $scope.currentUser.uid
+    }
+
+    var user = firebase.auth().currentUser;
+
+    $scope.selectedLocation = function (item) {
         alert("drop box item selected");
-    };
-
-    $scope.dropboxitemselected = function (item) {
-
         $scope.selectedLocation = item;
     };
 
-    $scope.update = function (user) {
-        alert("Бачка");
+    $scope.selectedCourse = function (item) {
+        alert("drop box item selected");
+        $scope.selectedCourse = item;
     };
+
+    $scope.update = function () {
+        console.log($scope.name);
+        user.updateProfile({
+            displayName: $scope.name
+        }).then(function () {
+            alert("Successfully changed your profile");
+        }, function (error) {
+            alert("An error occured " + error);
+        });
+    }
 });
 
