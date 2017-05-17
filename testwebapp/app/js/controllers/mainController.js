ae1bbf7416e7db1804f75289f3eb9399d04eb599
zulheika28 <zulheikadbatista@gmail.com>
2017-05-17 11:07:46 +0200

    Small changes

--- testwebapp/app/js/controllers/mainController.js
+++ testwebapp/app/js/controllers/mainController.js
@@ -5,8 +5,21 @@ angular.module('myApp').controller('MainController', ['$scope', 'SharingFactory'
 	
 	console.log($scope.IsSignedIn);
 
-	SharingFactory.setTeachers();
+	//SharingFactory.setTeachers();
 	SharingFactory.setSignedIn();
 
 	console.log($scope.teachers);
+
+	/**var isprofanity = require(['isprofanity']);
+
+	$scope.reviewComment = "You absolute vanker";//misspelling of 'wanker' which would be allowed by most filters... 
+ 
+	isprofanity($scope.reviewComment ,function(t){
+		console.log(t);
+    /*b = t ? 'contains' : 'does not contain';
+    console.log('"'+s+'" '+b+' profanity');*/
+   // process.exit();
+	//});
+
+
 }]);
---
+++ testwebapp/app/js/main.js
@@ -0,0 +1,10 @@
+var isProfanity = require('../../node_modules/isprofanity/isProfanity.js');
+var foo = require('./foo.js');
+var bar = require('../lib/bar.js');
+var gamma = require('gamma');
+
+var elem = document.getElementById('result');
+var x = foo(100) + bar('baz');
+elem.textContent = gamma(x);
+
+module.exports = function (n) { return n * 111 }
