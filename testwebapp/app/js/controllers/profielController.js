3f27427676a99cb663215c2e4aa27133bc7c16a3
Emily Timmerman <emilytimmerman@hotmail.co.uk>
2017-06-02 20:10:01 +0100

    User profile page, All of your reviews are now visible on the profile page

--- testwebapp/app/js/controllers/profielController.js
+++ testwebapp/app/js/controllers/profielController.js
@@ -1,9 +1,36 @@
-angular.module('myApp').controller('ProfileController', ['$scope', '$location', 'SharingFactory', function($scope, $location, SharingFactory){
+angular.module('myApp').controller('ProfileController', ['$scope', '$location', 'SharingFactory', function ($scope, $location, SharingFactory) {
 
-    SharingFactory.setUserData();
+	SharingFactory.setUserData();
 
 	$scope.username = SharingFactory.getUser();
-    $scope.userData = SharingFactory.getUserData();
+	$scope.userData = SharingFactory.getUserData();
 	$scope.tag = SharingFactory.getTagline();
 
+
+	$scope.userReviews = [];
+	$scope.limit = 2;
+
+	firebase.database().ref("Reviews").orderByChild("userID").equalTo($scope.userData.UserID).on('value', function (snapshot) {
+		snapshot.forEach(function (element) {
+			var value = element.val();
+
+			if (value.userID == $scope.userData.UserID) {
+				$scope.userReviews.push({
+					com: value.comment,
+					atmos: value.Atmosphere,
+					help: value.Helpfulness,
+					lec: value.Lectures,
+					prep: value.Preparation,
+					prof: value.Professionalism
+				});
+
+			}
+		});
+	});
+
+
+	$scope.more = function () {
+		$scope.limit = $scope.userReviews.length;
+	};
+	$scope.limit = $scope.userReviews.length;
 }]);
