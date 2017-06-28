c52fe9a2e5fe9ea30652bd22399ecc62b778b1c8
Anne <annemarieke-m@hotmail.com>
2017-06-28 21:44:14 +0200

    alert messages change

--- testwebapp/app/js/services/alertFactory.js
+++ testwebapp/app/js/services/alertFactory.js
@@ -1,12 +1,12 @@
 angular.module('myApp').factory('AlertFactory', [ function () {
     var alertFactory = {};
     var newRevSuccess = "Thank you! Your review has been added";
-    var reviewLimit = "You have already reviewd this teacher for during this module!";
+    var reviewLimit = "You have already reviewed this teacher during this module!";
     var ratingsFail = "Please fill in a star for each characteristic";
     var charFail = "Please be sure to have a minimum of 50 characters";
-    var verifyEMail = "Please verify your email and attempt login again";
-    var reportSuccess = "You successfully submitted your report. It will be reviewed by moderator.";
-    var reportFail = "There was a problem with submitting your review. Please Try again!";
+    var verifyEMail = "Please verify your email and attempt to login again";
+    var reportSuccess = "You successfully submitted your report. It will be reviewed by a moderator.";
+    var reportFail = "There was a problem with submitting your review. Please try again!";
 
 
     alertFactory.getNRS = function () {
